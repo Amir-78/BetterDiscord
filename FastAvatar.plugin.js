1deb2ea0132ec3aedace8bc8cae61174bdc63dcd
Amir <68391526+Amir-78@users.noreply.github.com>
2021-07-24 01:20:58 +0100

    1.0.3
    JQuery has been removed from the plugin.
    Some bugs has been fixed

--- FastAvatar.plugin.js
+++ FastAvatar.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name FastAvatar
  * @author Amir
- * @version 1.0.2
+ * @version 1.0.3
  * @description Show user avatar by clicking on profile picture.
  * @source https://github.com/Amir-78/BetterDiscord/blob/main/FastAvatar.plugin.js
  * @updateUrl https://raw.githubusercontent.com/Amir-78/BetterDiscord/main/FastAvatar.plugin.js
@@ -13,7 +13,7 @@ const config = {
         name: "FastAvatar",
         authors: [{ name: "Amir", github_username: "Amir-78", discord_id: "654357339691941899" }],
         description: "Show user avatar by clicking on profile picture.",
-        version: "1.0.2",
+        version: "1.0.3",
         github: "https://github.com/Amir-78/BetterDiscord/blob/main/FastAvatar.plugin.js",
         github_raw: "https://raw.githubusercontent.com/Amir-78/BetterDiscord/main/FastAvatar.plugin.js"
     },
@@ -21,7 +21,7 @@ const config = {
         {
             "title": "Fixed",
             "type": "fixed",
-            "items": ["Stop plugin problem has been fixed.", "Image quality has been fixed.", "Library required message has been fixed."]
+            "items": ["JQuery has been removed from the plugin.", "Some bugs has been fixed"]
         }
     ]
 };
@@ -37,36 +37,26 @@ class FastAvatar {
 
     load() {
 
-        if (!global.ZeresPluginLibrary){
+        if (!global.ZeresPluginLibrary) {
 
-			BdApi.showConfirmationModal(
-				"Library plugin is needed",
-				[`The library plugin needed for ${config.info.name} is missing. Please click Download to install it.`], {
-					confirmText: "Download",
-					cancelText: "Cancel",
-					onConfirm: () => {
-						require("request").get("https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js", async (error, response, body) => {
-							if (error) return require("electron").shell.openExternal("https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js");
-							await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, r));
-						});
-					}
-				}
-			);
+            BdApi.showConfirmationModal(
+                "Library plugin is needed",
+                [`The library plugin needed for ${config.info.name} is missing. Please click Download to install it.`], {
+                confirmText: "Download",
+                cancelText: "Cancel",
+                onConfirm: () => {
+                    require("request").get("https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js", async (error, response, body) => {
+                        if (error) return require("electron").shell.openExternal("https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js");
+                        await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, r));
+                    });
+                }
+            }
+            );
 
 
-        } 
+        }
         ZLibrary.PluginUpdater.checkForUpdate(this.getName(), this.getVersion(), this.getRaw());
 
-        let jQuery = document.getElementById("jQuery-FastAvatar")
-
-        if(!jQuery){
-        console.log("BetterDiscord FastAvatar: Loading jQuery...");
-        var script1 = document.createElement("script");
-        script1.id = "jQuery-FastAvatar"
-        script1.type = "text/javascript";
-        script1.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js";
-        document.head.appendChild(script1);
-        }
 
     }
 
@@ -83,10 +73,6 @@ class FastAvatar {
         document.removeEventListener("click", this.showAvatar);
         document.removeEventListener("click", this.hideAvatar);
 
-        let jQuery = document.getElementById("jQuery-FastAvatar")
-        if (jQuery) {
-            document.getElementById("jQuery-FastAvatar").remove();
-        }
     }
 
 
@@ -94,7 +80,65 @@ class FastAvatar {
         if (target.classList.contains("avatar-AvHqJA")) {
             let url = target.getElementsByTagName('img')[0].src;
 
-            $("body").prepend(`<div class="layerContainer-yqaFcK" id="FastAvatar-Avatar"><div class="FastAvatar-BackDrop backdrop-1wrmKB withLayer-RoELSG" style="opacity: 0.85; background: hsl(0, calc(var(--saturation-factor, 1) * 0%), 0%);"></div><div class="layer-2KE1M9"><div class="focusLock-Ns3yie" role="dialog" aria-label="Image" tabindex="-1" aria-modal="true"><div class="modal-qgFCbT root-1gCeng fullscreenOnMobile-1bD22y" style="opacity: 1; transform: scale(1);"><div class="wrapper-2K4Z3k"><div class="imageWrapper-2p5ogY image-1tIMwV" style="width: 128px; height: 128px;"><img alt="" src="${url.replace(/(?:\?size=\d{3,4})?$/, "?size=2048")}" style="width: 128px; height: 128px;"></div><a class="anchor-3Z-8Bb anchorUnderlineOnHover-2ESHQB downloadLink-1ywL9o" href="${url.replace(/(?:\?size=\d{3,4})?$/, "?size=2048")}" rel="noreferrer noopener" target="_blank" role="button" tabindex="0">Open original</a></div></div></div></div></div>`)
+            let check = document.getElementById("FastAvatar-Avatar");
+            if(check){
+            document.getElementById("FastAvatar-Avatar").remove();
+            }
+
+            var fastAvatarContainerDiv = document.createElement('div');
+            fastAvatarContainerDiv.className = "layerContainer-yqaFcK"
+            fastAvatarContainerDiv.id = "FastAvatar-Avatar"
+
+            var fastAvatarBackDrop = document.createElement('div');
+            fastAvatarBackDrop.className = "FastAvatar-BackDrop backdrop-1wrmKB withLayer-RoELSG"
+            fastAvatarBackDrop.style.cssText = "opacity: 0.85; background: hsl(0, calc(var(--saturation-factor, 1) * 0%), 0%);"
+
+            var fastAvatarLayer = document.createElement('div');
+            fastAvatarLayer.className = "layer-2KE1M9" 
+
+            var fastAvatarFocusLock = document.createElement('div');
+            fastAvatarFocusLock.className = "focusLock-Ns3yie" 
+            fastAvatarFocusLock.setAttribute('role', 'dialog');
+            fastAvatarFocusLock.setAttribute('aria-label', 'Image');
+            fastAvatarFocusLock.tabIndex = -1
+            fastAvatarFocusLock.setAttribute('aria-modal', 'true');
+
+            var fastAvatarModal = document.createElement('div');
+            fastAvatarModal.className = "modal-qgFCbT root-1gCeng fullscreenOnMobile-1bD22y" 
+            fastAvatarModal.style.cssText = "opacity: 1; transform: scale(1);"
+
+            var fastAvatarOpenLink = document.createElement('a');
+            fastAvatarOpenLink.className = "anchor-3Z-8Bb anchorUnderlineOnHover-2ESHQB downloadLink-1ywL9o"
+            fastAvatarOpenLink.rel = "noreferrer noopener"
+            fastAvatarOpenLink.target = "_blank"
+            fastAvatarOpenLink.tabIndex = 0
+            fastAvatarFocusLock.setAttribute('role', 'button');
+            fastAvatarOpenLink.text = "Open original"
+            fastAvatarOpenLink.href = url.replace(/(?:\?size=\d{3,4})?$/, "?size=2048");
+
+            var fastAvatarWrapper = document.createElement('div');
+            fastAvatarWrapper.className = "wrapper-2K4Z3k" 
+
+            var fastAvatarImageWrapper = document.createElement('div');
+            fastAvatarImageWrapper.className = "imageWrapper-2p5ogY image-1tIMwV" 
+            fastAvatarImageWrapper.style.cssText = "width: 128px; height: 128px;"
+
+            var fastAvatarImage = document.createElement('img');
+            fastAvatarImage.src = url.replace(/(?:\?size=\d{3,4})?$/, "?size=2048")
+            fastAvatarImage.style.cssText = "width: 128px; height: 128px;"
+
+            fastAvatarImageWrapper.appendChild(fastAvatarImage);
+            fastAvatarWrapper.appendChild(fastAvatarImageWrapper);
+            fastAvatarModal.appendChild(fastAvatarWrapper);
+            fastAvatarModal.appendChild(fastAvatarOpenLink);
+            fastAvatarFocusLock.appendChild(fastAvatarModal);
+            fastAvatarLayer.appendChild(fastAvatarFocusLock);
+            fastAvatarContainerDiv.appendChild(fastAvatarBackDrop);
+            fastAvatarContainerDiv.appendChild(fastAvatarLayer);
+            document.body.appendChild(fastAvatarContainerDiv);
+
+
+
 
         }
 
